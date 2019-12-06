6df1adaf81bdc460f0b8523e95eac610b1f9b661
Carl Hurd <carl@basilisklabs.com>
2019-12-05 20:45:39 -0500

    Addition of Day 5 CPU into Day 2, added parallelization for search, and rebench'd

--- src/day2/README.md
+++ src/day2/README.md
@@ -2,26 +2,67 @@
 ```
 AOC 2019
 Day 2 - Part 1 : 5866714
-        generator: 11.922µs,
-        runner: 7.911µs
+        generator: 4.9µs,
+        runner: 19µs
 
-Day 2 - Part 2 : 5208
-        generator: 6.705µs,
-        runner: 962.211µs
+Day 2 - Part 1 - rewrite : 5866714
+        generator: 300ns,
+        runner: 52.8µs
+
+Day 2 - Part 2 : 5208    
+        generator: 900ns,
+        runner: 12.1064ms
+
+Day 2 - Part 2 - clone_cpu : 5208
+        generator: 2.9µs,
+        runner: 2.2763ms
+
+Day 2 - Part 2 - double_iter : 5208
+        generator: 700ns,
+        runner: 202.9µs
+
+Day 2 - Part 2 - rewrite : 5208
+        generator: 8.8µs,
+        runner: 2.6891ms
 ```
 
 # Benchmarks
 ```
-Day2 - Part1/(default)  time:   [144.63 ns 145.14 ns 145.70 ns]                                   
-                        change: [-1.3330% +0.1676% +1.8653%] (p = 0.84 > 0.05)
-                        No change in performance detected.
-Found 11 outliers among 100 measurements (11.00%)
-  4 (4.00%) high mild
-  7 (7.00%) high severe
+Day2 - Part1/(default)  time:   [2.1298 us 2.1371 us 2.1449 us]
+Found 6 outliers among 100 measurements (6.00%)
+  5 (5.00%) high mild
+  1 (1.00%) high severe
 ```
 ```
-Day2 - Part2/(default)  time:   [708.24 us 710.37 us 712.55 us]                                   
-Found 14 outliers among 100 measurements (14.00%)
-  6 (6.00%) high mild
-  8 (8.00%) high severe
+Day2 - Part1/rewrite    time:   [5.5009 us 5.5223 us 5.5447 us]
+Found 7 outliers among 100 measurements (7.00%)
+  7 (7.00%) high mild
+```
+```
+Day2 - Part2/(default)  time:   [10.818 ms 10.834 ms 10.850 ms]
+Found 4 outliers among 100 measurements (4.00%)
+  3 (3.00%) high mild
+  1 (1.00%) high severe
+```
+```
+Day2 - Part2/clone_cpu  time:   [758.31 us 768.28 us 778.28 us]
+Found 3 outliers among 100 measurements (3.00%)
+  1 (1.00%) low mild
+  2 (2.00%) high mild
+```
+```
+Day2 - Part2/double_iter time:   [508.11 us 519.90 us 532.34 us]
+Found 10 outliers among 100 measurements (10.00%)
+  2 (2.00%) low severe
+  4 (4.00%) low mild
+  2 (2.00%) high mild
+  2 (2.00%) high severe
+```
+```
+Day2 - Part2/rewrite    time:   [1.2700 ms 1.2902 ms 1.3104 ms]
+Found 8 outliers among 100 measurements (8.00%)
+  1 (1.00%) low severe
+  3 (3.00%) low mild
+  3 (3.00%) high mild
+  1 (1.00%) high severe
 ```
--- src/day2/mod.rs
+++ src/day2/mod.rs
@@ -1,14 +1,18 @@
-#[aoc_generator(day2)]
-fn process_input(input: &str) -> Vec<usize> {
-    input
-        .split(",")
-        .map(|x| x.parse::<usize>().unwrap())
-        .collect()
-}
+// #[aoc_generator(day2)]
+// fn process_input(input: &str) -> Vec<usize> {
+//     input
+//         .split(",")
+//         .map(|x| x.parse::<usize>().unwrap())
+//         .collect()
+// }
 
 #[aoc(day2, part1)]
-fn d2p1(input: &Vec<usize>) -> usize {
-    let mut memory: Vec<usize> = input.to_vec();
+fn d2p1(input: &str) -> usize {
+    let mut memory: Vec<usize> = input
+            .trim()
+            .split(",")
+            .map(|x| x.parse::<usize>().unwrap())
+            .collect();
     let mut ip = 0;
     loop {
         let mut noun = memory[ip + 1];
@@ -40,10 +44,14 @@ fn d2p1(input: &Vec<usize>) -> usize {
 }
 
 #[aoc(day2, part2)]
-fn d2p2(input: &Vec<usize>) -> usize {
+fn d2p2(input: &str) -> usize {
     for o in 0..100 {
         for i in 0..100 {
-            let mut memory: Vec<usize> = input.to_vec();
+            let mut memory: Vec<usize> = input
+                .trim()
+                .split(",")
+                .map(|x| x.parse::<usize>().unwrap())
+                .collect();
             let mut ip = 0;
             memory[1] = o;
             memory[2] = i;
@@ -72,6 +80,98 @@ fn d2p2(input: &Vec<usize>) -> usize {
     unreachable!();
 }
 
+#[aoc(day2, part1, rewrite)]
+fn d2p1_rewrite(input: &str) -> isize {
+    let mut cpu = CPU::new(input);
+    cpu.set_memory(1, 12);
+    cpu.set_memory(2, 2);
+    if let Err(error) = cpu.run() {
+        println!("ERROR : {:?}", error);
+        0
+    } else {
+        cpu.get_memory(0)
+    }
+}
+
+#[aoc(day2, part2, rewrite)]
+fn d2p2_rewrite(input: &str) -> isize {
+    use rayon::prelude::*;
+    use std::sync::{Arc, atomic::{Ordering, AtomicIsize}};
+
+    let value = Arc::new(AtomicIsize::new(0));
+
+    (0 as isize..9999 as isize).into_par_iter().for_each(|x| {
+        if value.load(Ordering::SeqCst) == 0 {
+            let mut cpu = CPU::new(input);
+            cpu.set_memory(1, x / 100);
+            cpu.set_memory(2, x % 100);
+            if let Err(error) = cpu.run() {
+                
+            } else {
+                if cpu.get_memory(0) == 19690720 {
+                    value.store(x, Ordering::SeqCst)
+                }
+            }
+        }
+    });
+    
+    value.load(Ordering::SeqCst) as isize
+}
+
+#[aoc(day2, part2, clone_cpu)]
+fn d2p2_clone_cpu(input: &str) -> isize {
+    use rayon::prelude::*;
+    use std::sync::{Arc, atomic::{Ordering, AtomicIsize}};
+
+    let value = Arc::new(AtomicIsize::new(0));
+    let orig_cpu = CPU::new(input);
+
+    (0 as isize..9999 as isize).into_par_iter().for_each(|x| {
+        if value.load(Ordering::SeqCst) == 0 {
+            let mut cpu = orig_cpu.clone();
+            cpu.set_memory(1, x / 100);
+            cpu.set_memory(2, x % 100);
+            if let Err(error) = cpu.run() {
+                
+            } else {
+                if cpu.get_memory(0) == 19690720 {
+                    value.store(x, Ordering::SeqCst)
+                }
+            }
+        }
+    });
+    
+    value.load(Ordering::SeqCst) as isize
+}
+
+#[aoc(day2, part2, double_iter)]
+fn d2p2_double_iter(input: &str) -> isize {
+    use rayon::prelude::*;
+    use std::sync::{Arc, atomic::{Ordering, AtomicIsize}};
+
+    let value = Arc::new(AtomicIsize::new(0));
+    let orig_cpu = CPU::new(input);
+
+    (0 as isize..99 as isize).into_par_iter().for_each(|x| {
+        (0 as isize..99 as isize).into_par_iter().for_each(|y| {
+            if value.load(Ordering::SeqCst) == 0 {
+                let mut cpu = orig_cpu.clone();
+                cpu.set_memory(1, x);
+                cpu.set_memory(2, y);
+                if let Err(error) = cpu.run() {
+                    
+                } else {
+                    if cpu.get_memory(0) == 19690720 {
+                        value.store(x * 100 + y, Ordering::SeqCst)
+                    }
+                }
+            }
+        })   
+    });
+    
+    value.load(Ordering::SeqCst) as isize
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -81,3 +181,252 @@ mod tests {
         assert_eq!(d2p1(&process_input("1,9,10,3,2,3,11,0,99,30,40,50")), 3500);
     }
 }
+
+
+macro_rules! address_or_value {
+    ($iter_name:ident, $cpu:ident, $expression:expr) => {
+        if $iter_name.next() == Some('1') {
+            $cpu.get_memory($expression)
+        } else {
+            $cpu.get_value($expression)
+        }
+    };
+}
+
+type CpuResult<T> = std::result::Result<T, CpuError>;
+
+#[derive(Debug, Clone, Copy)]
+enum CpuError {
+    InvalidOpcode(isize, usize),
+    InvalidLastInstruction,
+    InvalidUserInput,
+}
+
+#[derive(Debug, Clone)]
+struct CPU {
+    memory: Vec<isize>,
+    instruction_pointer: usize,
+    last_instruction: Option<Instruction>,
+}
+
+impl CPU {
+    pub fn new(program: &str) -> CPU {
+        CPU {
+            memory: program
+                .trim()
+                .split(",")
+                .map(|x| x.parse::<isize>().unwrap())
+                .collect(),
+            instruction_pointer: 0,
+            last_instruction: None,
+        }
+    }
+
+    pub fn run(&mut self) -> CpuResult<()> {
+        loop {
+            match self.parse()? {
+                Instruction::Add(left, right, location) => {
+                    debug_print!("Add : {} + {} @ {}", left, right, location);
+                    self.last_instruction = Some(Instruction::Add(left, right, location));
+                    self.set_memory(location as usize, left + right);
+                }
+                Instruction::Mult(left, right, location) => {
+                    debug_print!("Mult : {} * {} @ {}", left, right, location);
+                    self.last_instruction = Some(Instruction::Mult(left, right, location));
+                    self.set_memory(location as usize, left * right);
+                }
+                Instruction::In(location) => {
+                    debug_print!("In : @ {}", location);
+                    self.last_instruction = Some(Instruction::In(location));
+                    self.set_memory(location as usize, get_input()?);
+                }
+                Instruction::Out(value) => {
+                    debug_print!("Out : {}", value);
+                    self.last_instruction = Some(Instruction::Out(value));
+                    println!("{}", value as usize);
+                }
+                Instruction::JumpIfTrue(value, new_ip) => {
+                    debug_print!("JIT : {} to {}", value, new_ip);
+                    self.last_instruction = Some(Instruction::JumpIfTrue(value, new_ip));
+                    if value != 0 {
+                        self.instruction_pointer = new_ip as usize;
+                    }
+                }
+                Instruction::JumpIfFalse(value, new_ip) => {
+                    debug_print!("JIF : {} to {}", value, new_ip);
+                    self.last_instruction = Some(Instruction::JumpIfFalse(value, new_ip));
+                    if value == 0 {
+                        self.instruction_pointer = new_ip as usize;
+                    }
+                }
+                Instruction::LessThan(left, right, location) => {
+                    debug_print!("LT : {} < {} @ {}", left, right, location);
+                    self.last_instruction = Some(Instruction::LessThan(left, right, location));
+                    if left < right {
+                        self.set_memory(location as usize, 1);
+                    } else {
+                        self.set_memory(location as usize, 0);
+                    }
+                }
+                Instruction::Equal(left, right, location) => {
+                    debug_print!("EQ : {} < {} @ {}", left, right, location);
+                    self.last_instruction = Some(Instruction::Equal(left, right, location));
+                    if left == right {
+                        self.set_memory(location as usize, 1);
+                    } else {
+                        self.set_memory(location as usize, 0);
+                    }
+                }
+                Instruction::Halt => {
+                    debug_print!("Halt");
+                    self.last_instruction = Some(Instruction::Halt);
+                    return Ok(());
+                }
+            }
+            self.increment_ip()?;
+        }
+    }
+
+    fn parse(&self) -> CpuResult<Instruction> {
+        Instruction::parse(&self)
+    }
+
+    fn increment_ip(&mut self) -> CpuResult<()> {
+        if let Some(last_instr) = self.last_instruction {
+            match last_instr {
+                Instruction::Add(_, _, _) => {
+                    self.instruction_pointer += 4;
+                }
+                Instruction::Mult(_, _, _) => {
+                    self.instruction_pointer += 4;
+                }
+                Instruction::In(_) => {
+                    self.instruction_pointer += 2;
+                }
+                Instruction::Out(_) => {
+                    self.instruction_pointer += 2;
+                }
+                Instruction::JumpIfTrue(value, _) => {
+                    if value != 0 {
+                    } else {
+                        self.instruction_pointer += 3;
+                    }
+                }
+                Instruction::JumpIfFalse(value, _) => {
+                    if value == 0 {
+                    } else {
+                        self.instruction_pointer += 3;
+                    }
+                }
+                Instruction::LessThan(_, _, _) => {
+                    self.instruction_pointer += 4;
+                }
+                Instruction::Equal(_, _, _) => {
+                    self.instruction_pointer += 4;
+                }
+                Instruction::Halt => {
+                    self.instruction_pointer += 1;
+                }
+            }
+            Ok(())
+        } else {
+            Err(CpuError::InvalidLastInstruction)
+        }
+    }
+
+    /// Get the value at a memory address
+    fn get_memory(&self, address: usize) -> isize {
+        self.memory[address]
+    }
+
+    /// Convienient wrapper around `get_memory` that will get a value instead of a position if needed
+    fn get_value(&self, address: usize) -> isize {
+        self.get_memory(self.get_memory(address) as usize)
+    }
+
+    fn set_memory(&mut self, address: usize, value: isize) {
+        self.memory[address] = value;
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Instruction {
+    Add(isize, isize, isize),
+    Mult(isize, isize, isize),
+    In(isize),
+    Out(isize),
+    JumpIfTrue(isize, isize),
+    JumpIfFalse(isize, isize),
+    LessThan(isize, isize, isize),
+    Equal(isize, isize, isize),
+    Halt,
+}
+
+impl Instruction {
+    fn parse(cpu: &CPU) -> CpuResult<Instruction> {
+        let mem = cpu.get_memory(cpu.instruction_pointer);
+        let flags = (mem / 100).to_string();
+        let mut flags_iter = flags.chars().rev();
+        let instr = match mem % 100 {
+            1 => Instruction::Add(
+                address_or_value!(flags_iter, cpu, cpu.instruction_pointer + 1),
+                address_or_value!(flags_iter, cpu, cpu.instruction_pointer + 2),
+                cpu.get_memory(cpu.instruction_pointer + 3),
+            ),
+            2 => Instruction::Mult(
+                address_or_value!(flags_iter, cpu, cpu.instruction_pointer + 1),
+                address_or_value!(flags_iter, cpu, cpu.instruction_pointer + 2),
+                cpu.get_memory(cpu.instruction_pointer + 3),
+            ),
+            3 => Instruction::In(cpu.get_memory(cpu.instruction_pointer + 1)),
+            4 => Instruction::Out(address_or_value!(
+                flags_iter,
+                cpu,
+                cpu.instruction_pointer + 1
+            )),
+            5 => Instruction::JumpIfTrue(
+                address_or_value!(flags_iter, cpu, cpu.instruction_pointer + 1),
+                address_or_value!(flags_iter, cpu, cpu.instruction_pointer + 2),
+            ),
+            6 => Instruction::JumpIfFalse(
+                address_or_value!(flags_iter, cpu, cpu.instruction_pointer + 1),
+                address_or_value!(flags_iter, cpu, cpu.instruction_pointer + 2),
+            ),
+            7 => Instruction::LessThan(
+                address_or_value!(flags_iter, cpu, cpu.instruction_pointer + 1),
+                address_or_value!(flags_iter, cpu, cpu.instruction_pointer + 2),
+                cpu.get_memory(cpu.instruction_pointer + 3),
+            ),
+            8 => Instruction::Equal(
+                address_or_value!(flags_iter, cpu, cpu.instruction_pointer + 1),
+                address_or_value!(flags_iter, cpu, cpu.instruction_pointer + 2),
+                cpu.get_memory(cpu.instruction_pointer + 3),
+            ),
+            99 => Instruction::Halt,
+            _ => {
+                return Err(CpuError::InvalidOpcode(
+                    cpu.get_memory(cpu.instruction_pointer),
+                    cpu.instruction_pointer,
+                ))
+            }
+        };
+
+        Ok(instr)
+    }
+}
+
+fn get_input() -> CpuResult<isize> {
+    use std::io;
+
+    let mut input = String::new();
+
+    if let Err(_) = io::stdin().read_line(&mut input) {
+        return Err(CpuError::InvalidUserInput);
+    }
+    let parse_input = input.trim().parse::<isize>();
+    match parse_input {
+        Err(_) => return Err(CpuError::InvalidUserInput),
+        Ok(val) => return Ok(val),
+    }
+}
+
